4012911db30e4874802f875e5552a0c05919300a
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-10-01 08:54:31 -0300

    CheckedException

--- src/br/com/dio/exceptions/CheckedException.java
+++ src/br/com/dio/exceptions/CheckedException.java
@@ -5,16 +5,28 @@ import java.io.*;
 
 //Imprimir um arquivo no console.
 public class CheckedException {
-    /*public static void main(String[] args)  {
-        String nomeDoArquivo = "romances-blake-crouch.txt";
-        imprimirArquivoNoConsole(nomeDoArquivo);
+    public static void main(String[] args) {
+        String nomeDoArquivo = "romance-blake-crouch.txt";
+
+        try {
+            imprimirArquivoNoConsole(nomeDoArquivo);
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null,
+                    "Revise o nome do arquivo que você deseja imprimir! " + e.getCause());
+            e.printStackTrace();
+        } catch (IOException e){
+            //e.printStackTrace();
+            JOptionPane.showMessageDialog(null,
+                    "Ocorreu um erro inesperado! Entre em contato com o suporte! " + e.getCause());
+        } finally {
+            System.out.println("Chegou no finally!");
+        }
 
         System.out.println("Apesar da exception ou não, o programa continua...");
     }
 
-    public static void imprimirArquivoNoConsole(String nomeDoArquivo) {
+    public static void imprimirArquivoNoConsole(String nomeDoArquivo) throws IOException {
         File file = new File(nomeDoArquivo);
-
 
         BufferedReader br = new BufferedReader(new FileReader(file.getName()));
         String line = br.readLine();
@@ -28,5 +40,5 @@ public class CheckedException {
         } while(line != null);
         bw.flush();
         br.close();
-    }*/
+    }
 }

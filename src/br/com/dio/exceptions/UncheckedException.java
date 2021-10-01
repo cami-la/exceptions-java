0d336a18e7e7c06bf2238c85e2a65f229e08c669
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-10-01 07:11:40 -0300

    UncheckedException

--- src/br/com/dio/exceptions/UncheckedException.java
+++ src/br/com/dio/exceptions/UncheckedException.java
@@ -5,14 +5,33 @@ import javax.swing.*;
 //Fazer a divisão de 2 valores inteiros
 public class UncheckedException {
     public static void main(String[] args) {
-        String a = JOptionPane.showInputDialog("Numerador: ");
-        String b = JOptionPane.showInputDialog("Denominador: ");
 
-        double resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
-        System.out.println("Resultado: " + resultado);
+        boolean continueLooping = true;
+        do {
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+
+            try{
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+                System.out.println("Resultado: " + resultado);
+                continueLooping = false;
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Entrada inválida, informe um número inteiro! " + e.getMessage());
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Impossível dividir um número por 0.");
+            }
+            finally {
+                System.out.println("Chegou no finally!");
+            }
+        } while(continueLooping);
+
+
+        System.out.println("O código continua...");
     }
 
-    public static double dividir(int a, int b) {
-        return (double) a / b;
+    public static int dividir(int a, int b) {
+        return a / b;
     }
 }

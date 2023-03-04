85e57188962bdfc4a4376c50f211dd3aa46738dc
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2023-03-04 14:31:33 -0300

    Criando Exceção Customizada

--- src/br/com/dio/exceptions/ExceptionCustomizada_2.java
+++ src/br/com/dio/exceptions/ExceptionCustomizada_2.java
@@ -1,24 +1,38 @@
 package br.com.dio.exceptions;
 
 import javax.swing.*;
+import java.util.Arrays;
 
 public class ExceptionCustomizada_2 {
     public static void main(String[] args){
         int[] numerador = {4, 5, 8, 10};
         int[] denominador = {2, 4, 0, 2, 8};
 
+
         for (int i = 0; i < denominador.length; i++) {
-            try {
-                if(numerador[i] %2 != 0)
-                    throw new DivisaoNaoExataException("Divisão não exata!", numerador[i], denominador[i]);
+            try{
+                if(denominador[i] == 0)
+                    throw new DivisaoPorZeroException("Divisao por Zero",denominador[i]);
+                try {
+                    if (numerador[i] % denominador[i] != 0)
+                        throw new DivisaoNaoExataException("Divisão não exata!", numerador[i], denominador[i]);
+                }catch(ArrayIndexOutOfBoundsException e){
+                    throw new TamanhoDoArrayInvalidoException("Tamanho do Array Invalido", i);
+                }
 
                 int resultado = numerador[i] / denominador[i];
                 System.out.println(resultado);
-            } catch (DivisaoNaoExataException | ArithmeticException | ArrayIndexOutOfBoundsException e) {
-                e.printStackTrace();
+
+
+            } catch (DivisaoNaoExataException | DivisaoPorZeroException | TamanhoDoArrayInvalidoException e) {
+                // e.printStackTrace();
                 JOptionPane.showMessageDialog(null, e.getMessage());
             }
+
+
+
         }
+
 
         System.out.println("O programa continua...");
     }
